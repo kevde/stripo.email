851439778b8a05823bb889a9c239df30f5f2674f
kevde <aronkev@gmailcom>
2019-08-20 18:02:45 +0800

    Added properties for API binding

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const fromCdn = require('from-cdn');
-const stripoCdn = 'https://plugins.stripo.email/static/latest/stripo.js';
+const axios = require('axios');
+const CDN_PATH = 'https://plugins.stripo.email/static/latest/stripo.js';
+const AUTH_PATH = 'https://plugins.stripo.email/api/v1/auth';
 
 class StripoWrapper {
   constructor() {
     this.Stripo = null;
   }
   async load() {
-    const results = await fromCdn([stripoCdn]);
+    const results = await fromCdn([CDN_PATH]);
     this.Stripo = (window.Stripo) ? window.Stripo : results[0];
     return this;
   }
 
-  async init(stripoSettings) {
+  async init({
+    pluginId,
+    secretKey,
+    ...stripoSettings
+  }) {
+    const pluginId = st
     if (!this.Stripo) {
       await this.load();
     }
-    this.Stripo.init(stripoSettings);
+    this.Stripo.init({
+      ...stripoSettings,
+      getAuthToken: this.getAuthTokenFunc(pluginId, secretKey),
+    });
+  }
+
+  async getAuthToken(pluginId, secretKey) {
+    const response = await axios.post(AUTH_PATH, {
+      pluginId,
+      secretKey
+    });
+    return (callback) => calback(response.token);
   }
 
   isLoaded() {
